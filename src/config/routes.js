ff8894d969cd26bcd80d467429e217e9518d50f0
zlxiao97 <zlxiao97@yahoo.com>
2024-09-25 17:46:27 +0800

    👌 IMPROVE: 删除 demo 页

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,15 +1,9 @@
 import HomePage from '@/pages/HomePage.vue'
-import AboutPage from '@/pages/AboutPage.vue'
 
 export default [
   {
     path: '/',
     name: 'Home',
     component: HomePage
-  },
-  {
-    path: '/about',
-    name: 'About',
-    component: AboutPage
   }
 ]
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,32 +1,10 @@
 <script setup>
-import { useAppStore } from '@/stores/app';
-import { storeToRefs } from 'pinia';
-
-const appStore = useAppStore()
-
-const { name } = storeToRefs(appStore)
-
 </script>
 
 <template>
-  <div class="home-page">
-    <el-text
-      class="mx-1"
-      type="primary"
-    >
-      Home Page
-    </el-text>
-    <p>Welcome to the {{ name }} Home Page</p>
-    <router-link to="/about">
-      Go to About Page
-    </router-link>
-  </div>
+  <div />
 </template>
 
 <style lang="less" scoped>
-.home-page {
-  h1 {
-    color: #3498db;
-  }
-}
+
 </style>
